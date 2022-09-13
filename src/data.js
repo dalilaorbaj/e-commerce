55f2dad48d0675696a50365f564fd6ec1f2eb6a7
dalilaorbaj <dalila.orbaj@gmail.com>
2022-09-13 08:22:15 -0300

    Images being showed

--- src/data.js
+++ src/data.js
@@ -1,7 +1,7 @@
 const productos = [
     { 
     id: 1,
-    imagen: "./images/product-1.png", 
+    imagen: "../images/product-1.png", 
     titulo: "Nordic chair", 
     descripcion: "Como base, lleva una estructura metálica muy resistente, con acabado en negro. La silla está tapizada en poliéster suave y se puede adquirir en varios colores alegres, que aportarán un toque desenfadado y único a tu salón.", 
     precio: 15499 
@@ -9,14 +9,14 @@ const productos = [
 /*todo lo que esta en public es public por lo que no es necesario poner public*/  
     { 
     id: 2,
-    imagen: "./images/product-2.png", 
+    imagen: "../images/product-2.png", 
     titulo: "Kruzo Aero Chair", 
     descripcion: "Como base, lleva una estructura metálica muy resistente, con acabado en negro. La silla está tapizada en poliéster suave y se puede adquirir en varios colores alegres, que aportarán un toque desenfadado y único a tu salón.", 
     precio: 11299 
     },
     { 
     id: 3,
-    imagen: "./images/product-3.png", 
+    imagen: "../images/product-3.png", 
     titulo: "Ergonomic Chair", 
     descripcion: "Como base, lleva una estructura metálica muy resistente, con acabado en negro. La silla está tapizada en poliéster suave y se puede adquirir en varios colores alegres, que aportarán un toque desenfadado y único a tu salón.", 
     precio: 18599 
