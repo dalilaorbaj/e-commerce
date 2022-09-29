58477dd6b6c90c8632861aaee31b56879a1e52ef
Android Studio <devandroid@ort>
2022-09-29 09:59:37 -0300

    For real all donegit status

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -4,37 +4,44 @@ import Articulo from "./Articulo";
 import { CartContext } from '../Context/cartContext';
 
 const Cart = () => {
-  const {carrito, getTotalCarrito} = useContext(CartContext);
+  const { carrito, getTotalCarrito } = useContext(CartContext);
+  const [total, setTotal] = useState(0);
 
-  // TODO: Implementar getTotalCarrito() usando un state y un useEffect.
-  // El useState guarda en una var totalCarrito.
-  // un use effect observa sobre carrito y renueva la variable totalCarrito
+  useEffect(() => {
+    let tot = getTotalCarrito();
+    setTotal(tot);
+  });
 
   return (
-
     <>
       <div className="untree_co-section before-footer-section">
         <div className="container">
           <div className="row mb-5">
+
             <form className="col-md-12 readOnly" method="post">
               <div className="site-blocks-table">
                 <table className="table">
                   <thead>
-                    <tr>
-                      <th className="product-thumbnail">Image</th>
-                      <th className="product-name">Product</th>
-                      <th className="product-price">Price</th>
-                      <th className="product-quantity">Quantity</th>
-                      <th className="product-total">Total</th>
-                      <th className="product-remove">Remove</th>
-                    </tr>
+{
+  total === 0 ? 
+                      <h1>No hay productos aún!</h1>: 
+
+
+                      <tr>
+                        <th className="product-thumbnail">Imagen</th>
+                        <th className="product-name">Producto</th>
+                        <th className="product-price">Precio</th>
+                        <th className="product-quantity">Cantidad</th>
+                        <th className="product-total">Total</th>
+                        <th className="product-remove">Eliminar</th>
+                      </tr>}
                   </thead>
                   <tbody>
                     {carrito.map((item) => (
                       <Articulo
                         key={item.id}
                         prod={item}
-                        />
+                      />
                     ))}
                   </tbody>
                 </table>
@@ -47,20 +54,20 @@ const Cart = () => {
               <div className="row mb-5">
                 <div className="col-md-6">
                   <Link to="/">
-                    <button className="btn btn-outline-black btn-sm btn-block">Continue Shopping</button>
+                    <button className="btn btn-outline-black btn-sm btn-block">Continuar comprando</button>
                   </Link>
                 </div>
               </div>
               <div className="row">
                 <div className="col-md-12">
-                  <label className="text-black h4" htmlFor="coupon">Coupon</label>
-                  <p>Enter your coupon code if you have one.</p>
+                  <label className="text-black h4" htmlFor="coupon">Cupon</label>
+                  <p>Ingrese su cupón si tiene uno.</p>
                 </div>
                 <div className="col-md-8 mb-3 mb-md-0">
-                  <input type="text" className="form-control py-3" id="coupon" placeholder="Coupon Code" />
+                  <input type="text" className="form-control py-3" id="coupon" placeholder="Codigo del cupón" />
                 </div>
                 <div className="col-md-4">
-                  <button className="btn btn-black">Apply Coupon</button>
+                  <button className="btn btn-black">Aplicar Cupon</button>
                 </div>
               </div>
             </div>
@@ -69,15 +76,7 @@ const Cart = () => {
                 <div className="col-md-7">
                   <div className="row">
                     <div className="col-md-12 text-right border-bottom mb-5">
-                      <h3 className="text-black h4 text-uppercase">Cart Totals</h3>
-                    </div>
-                  </div>
-                  <div className="row mb-3">
-                    <div className="col-md-6">
-                      <span className="text-black">Subtotal</span>
-                    </div>
-                    <div className="col-md-6 text-right">
-                      <strong className="text-black">$230.00</strong>
+                      <h3 className="text-black h4 text-uppercase">Total del carrito</h3>
                     </div>
                   </div>
                   <div className="row mb-5">
@@ -85,13 +84,13 @@ const Cart = () => {
                       <span className="text-black">Total</span>
                     </div>
                     <div className="col-md-6 text-right">
-                      <strong className="text-black">$230.00</strong>
+                      <strong className="text-black">${total}.00</strong>
                     </div>
                   </div>
 
                   <div className="row">
                     <div className="col-md-12">
-                      <button className="btn btn-black btn-lg py-3 btn-block">Proceed To Checkout</button>
+                      <button className="btn btn-black btn-lg py-3 btn-block">Ir a Checkout</button>
                     </div>
                   </div>
                 </div>
